269d3b7c840f48e28110bcc5aebecba0d836f7b1
Shahbaz Khan <mailshahbazkhan@gmail.com>
2019-03-30 23:16:44 +0530

    Update assignment5
    fixed the bug for input not in range of 0 to 1

--- Python Programming for Everybody/Assignment/Assignment5.py
+++ Python Programming for Everybody/Assignment/Assignment5.py
@@ -1,12 +1,14 @@
 score = float(input("Enter Score: "))
-if(score>=0.9):
+if(score<=1 and score>=0.9):
     grade = "A"
-elif(score>=0.8):
+elif(score<0.9 and score>=0.8):
     grade = "B"
-elif(score>=0.7):
+elif(score<0.8 and score>=0.7):
     grade = "C"
-elif(score>=0.6):
+elif(score<0.7 and score>=0.6):
     grade = "D"
-else:
+elif(score<0.6 and score>=0):
     grade = "F"
+else:
+    grade = "an error"
 print(grade)
